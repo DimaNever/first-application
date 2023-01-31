ff42600dc33c060cb589399d98e76cae1c0527b4
Dmitrii Neverov <dimanever86@mail.ru>
2023-01-31 14:30:10 +0300

    123

--- src/com/dmdev/cs/lesson15/Lesson15.java
+++ src/com/dmdev/cs/lesson15/Lesson15.java
@@ -1,2 +1,7 @@
-package com.dmdev.cs.lesson15;public class Lesson15 {
+package com.dmdev.cs.lesson15;
+
+public class Lesson15 {
+    public static void main(String[] args) {
+        System.out.println("dfdf");
+    }
 }
